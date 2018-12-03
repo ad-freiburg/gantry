2597db3550f68f1759162aa255c2bd5f5cecd950
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2018-12-03 10:14:49 +0100

    Use new nameschema for gantry files

--- constants.go
+++ constants.go
@@ -1,6 +1,6 @@
 package gantry // import "github.com/ad-freiburg/gantry"
 
 const DockerCompose string = "docker-compose.yml"
-const GantryDef string = "Gantrydef"
-const GantryEnv string = "Gantryenv"
+const GantryDef string = "gantry.def.yml"
+const GantryEnv string = "gantry.env.yml"
 const Version = "0.0.1a1"
