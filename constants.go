dd0e8e60346a9bc9c3cd3dee83b312318e365f4b
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2019-01-23 10:20:49 +0100

    Add global gnatry logger

--- constants.go
+++ constants.go
@@ -1,6 +1,22 @@
 package gantry // import "github.com/ad-freiburg/gantry"
 
+import (
+	"log"
+	"os"
+)
+
 const DockerCompose string = "docker-compose.yml"
 const GantryDef string = "gantry.def.yml"
 const GantryEnv string = "gantry.env.yml"
 const Version = "0.0.1a1"
+
+var (
+	gantryLogger *PrefixedLogger
+)
+
+func init() {
+	pipelineLogger = NewPrefixedLogger(
+		ApplyStyle("gantry", STYLE_BOLD),
+		log.New(os.Stderr, "", log.LstdFlags),
+	)
+}
