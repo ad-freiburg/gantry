1e43ee490b68c1dab2641ff45285f0071f59564c
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2019-02-07 10:32:32 +0100

    TestPipelineDefinitionPipelines added

--- pipeline_test.go
+++ pipeline_test.go
@@ -19,7 +19,7 @@ func TestPipelinesAllSteps(t *testing.T) {
 	for _, c := range cases {
 		r := c.input.AllSteps()
 		if !reflect.DeepEqual(r, c.result) {
-			t.Errorf("Incorrect result for '%v', got: %#v, wanted %#v", c.input, r, c.result)
+			t.Errorf("Incorrect result for '%v', got: '%#v', wanted '%#v'", c.input, r, c.result)
 		}
 	}
 }
@@ -49,7 +49,35 @@ func TestPipelinesCheck(t *testing.T) {
 	for _, c := range cases {
 		r := c.input.Check()
 		if (r == nil && c.result != "") || (r != nil && c.result == "") {
-			t.Errorf("Incorrect result for '%v', got: %s, wanted %s", c.input, r, c.result)
+			t.Errorf("Incorrect result for '%v', got: '%s', wanted '%s'", c.input, r, c.result)
+		}
+	}
+}
+
+func TestPipelineDefinitionPipelines(t *testing.T) {
+	cases := []struct {
+		definition gantry.PipelineDefinition
+		err        string
+		result     gantry.Pipelines
+	}{
+		{gantry.PipelineDefinition{}, "", gantry.Pipelines{}},
+		{gantry.PipelineDefinition{Steps: gantry.StepList{"a": gantry.Step{}}}, "", gantry.Pipelines{[]gantry.Step{gantry.Step{}}}},
+		{gantry.PipelineDefinition{Services: gantry.ServiceList{"a": gantry.Step{}}}, "", gantry.Pipelines{[]gantry.Step{gantry.Step{}}}},
+		{gantry.PipelineDefinition{Steps: gantry.StepList{"a": gantry.Step{}}, Services: gantry.ServiceList{"a": gantry.Step{}}}, "Redeclaration of step 'a'", gantry.Pipelines{}},
+	}
+
+	for _, c := range cases {
+		r, err := c.definition.Pipelines()
+		if (err == nil && c.err != "") || (err != nil && c.err == "") {
+			t.Errorf("Incorrect error for '%v', got: '%s', wanted '%s'", c.definition, err, c.err)
+		}
+		if err != nil {
+			continue
+		}
+		for i, ri := range *r {
+			if len(ri) != len(c.result[i]) {
+				t.Errorf("Incorrect length for '%v'@'%d', got: '%d', wanted: '%d'", c.definition, i, len(ri), len(c.result[i]))
+			}
 		}
 	}
 }
