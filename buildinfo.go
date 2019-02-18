eabed79eeba4e721d41820eb8da9f6b47cfaf66b
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2019-02-18 09:11:03 +0100

    more comments

--- buildinfo.go
+++ buildinfo.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ad-freiburg/gantry/types"
 )
 
+// BuildInfo represents the build-keyword in a docker-compse.yml.
 type BuildInfo struct {
 	Context    string                  `json:"context"`
 	Dockerfile string                  `json:"dockerfile"`
