828b111445a83621215925dc37cf3c0c1b097d4d
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2019-06-25 18:42:34 +0200

    gantry down is now more like docker-compose down

--- cmd/down.go
+++ cmd/down.go
@@ -12,10 +12,14 @@ var downCmd = &cobra.Command{
 	Use:   "down",
 	Short: "Stop and remove containers, and networks created by `up`",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		err := killCmd.RunE(cmd, args)
-		if err != nil {
+		// This is a docker-compose only command, do not try to clean up TempDirs
+		pipeline.Environment.TempDirNoAutoClean = true
+		if err := killCmd.RunE(cmd, args); err != nil {
 			return err
 		}
-		return rmCmd.RunE(cmd, args)
+		if err := rmCmd.RunE(cmd, args); err != nil {
+			return err
+		}
+		return pipeline.RemoveNetwork()
 	},
 }
