36d62ed4d12fd88c5f5a00c92af807cb68ceeeaa
Axel Lehmann <lehmann@cs.uni-freiburg.de>
2019-06-25 19:39:15 +0200

    Correctly handle CleanUp for special steps

--- cmd/down.go
+++ cmd/down.go
@@ -12,8 +12,6 @@ var downCmd = &cobra.Command{
 	Use:   "down",
 	Short: "Stop and remove containers, and networks created by `up`",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// This is a docker-compose only command, do not try to clean up TempDirs
-		pipeline.Environment.TempDirNoAutoClean = true
 		if err := killCmd.RunE(cmd, args); err != nil {
 			return err
 		}
@@ -22,4 +20,5 @@ var downCmd = &cobra.Command{
 		}
 		return pipeline.RemoveNetwork()
 	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {},
 }
